16c7de9d4194407d1d31a5bf33f80f03fa3d8869
susangi-admin <susangi@mylinex.com>
2022-09-13 15:12:55 +0530

    create : create basic crud operatios nodejs >> using express >> show,find,add,update,delete

---
+++ Users.js
@@ -0,0 +1,19 @@
+const  users = [
+	{
+		id : 1,
+		name : 'John',
+		email : 'susi@gmail.com'
+	},
+	{
+		id : 2,
+		name : 'Smith',
+		email : 'smith@gmail.com'
+	},
+	{
+		id : 3,
+		name : 'CLes',
+		email : 'ckes@gmail.com'
+	},
+]
+
+module.exports = users;
